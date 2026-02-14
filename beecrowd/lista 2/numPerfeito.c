d7149ff3197598fa679a4b50f4d06ab554db1cc8
lucaphill <llucaphill@gmail.com>
2026-02-14 17:58:35 -0300

    numPerfeito

--- beecrowd/lista 2/numPerfeito.c
+++ beecrowd/lista 2/numPerfeito.c
@@ -8,10 +8,33 @@ int main(void)
 {
     // definindo variaveis
     int n, m, soma;
+    soma = 0;
+    n = 0;
+    m = 0;
+
     // imput
     scanf("%d", &n);
     // calculo
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-    }
+        scanf("%d", &m);
+        soma = 0;
+
+        for (int j = 1; j < m; j++)
+        {
+            if (m % j == 0)
+            {
+                soma = soma + j;
+            };
+        };
+
+        if (soma == m)
+        {
+            printf("%d eh perfeito\n", m);
+        }
+        else
+        {
+            printf("%d nao eh perfeito\n", m);
+        };
+    };
 }
