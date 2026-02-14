2c7fcdfe5f5594ce9e8694033aa3145bb87b1d31
lucaphill <llucaphill@gmail.com>
2026-02-14 18:18:02 -0300

    numPrimos

---
+++ beecrowd/lista 2/numPrimo.c
@@ -0,0 +1,34 @@
+/*
+Luca Borges
+Projeto: beecrowd lista 2-numeros primos
+*/
+#include <stdio.h>
+
+int main(void){
+    //definindo variaveis
+    int teste, valor,soma;
+    soma = 0;
+
+    //imput
+    scanf("%d",&teste);
+    //programa
+    for (int i=1; i <= teste; i++){
+        scanf("%d",&valor);
+        soma = 0;
+        for (int j = 1; j < valor; j++){
+
+            if (valor%j == 0){
+                soma = soma + j;
+            };
+        };
+
+        if (soma == 1){
+            printf("%d eh primo\n",valor);
+        } else {
+            printf("%d nao eh primo\n", valor);
+
+        };
+
+    };
+
+}
