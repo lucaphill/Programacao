4c394d851b3b8ee5984d8475b06d0bf5511cef49
lucaphill <llucaphill@gmail.com>
2026-02-14 17:24:39 -0300

    Comite incial

---
+++ beecrowd/lista 2/divisores1.c
@@ -0,0 +1,22 @@
+/*
+Luca borges
+Projeto: beecrowd lista 2-divisores
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // definindo variaveis
+    int valor, divisor;
+    // pegando valor
+    scanf("%d", &valor);
+    // calculo
+    for (int i = 1; i <= valor; i++)
+    {
+        if (valor % i == 0)
+        {
+            divisor = i;
+            printf("%d\n", divisor);
+        };
+    };
+}
---
+++ beecrowd/lista 2/numPerfeito.c
@@ -0,0 +1,17 @@
+/*
+Luca borges
+Projeto: beecrowd lista 2-numero perfeito
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // definindo variaveis
+    int n, m, soma;
+    // imput
+    scanf("%d", &n);
+    // calculo
+    for (int i = 1; i < n; i++)
+    {
+    }
+}
