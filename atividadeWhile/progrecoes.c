7461ccd88daaf8d80ea9f1353189637a3c0bba92
lucaphill <llucaphill@gmail.com>
2026-02-16 22:59:09 -0300

    identificar se o num é primo

---
+++ atividadeWhile/progrecoes.c
@@ -0,0 +1,21 @@
+/*
+Luca Borges
+Programa: um programa que le a entrada do usuario e faz uma progreção aritmetica/geometrica
+*/
+#include<stdio.h>
+int main(void){
+    int c, t, r, n;
+
+    puts("Voce quer fazer uma progressao aritmetica(a) ou geometrica (g)?");
+    scanf("%d",&c);
+
+    if ( c == "a"){
+
+
+
+    };
+
+
+
+    return 0;
+}
