16e8d3337cae95aa93ff37aa3b35049075879324
lucaphill <llucaphill@gmail.com>
2026-02-16 22:12:17 -0300

    programa que escreve frases 100 vezes

---
+++ atividadeWhile/detencao.c
@@ -0,0 +1,13 @@
+/*
+Luca Borges
+Programa: escreve 100 vezes frases sobre programação kkk
+*/
+#include<stdio.h>
+int main (void){
+    for(int i = 1; i <= 100; i ++){
+        puts("So aprende a programar quem escreve programas");
+        puts("Quem nao escreve programas nao aprende a programar");
+    };
+
+    return 0;
+}
