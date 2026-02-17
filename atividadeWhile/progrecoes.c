c53280e1fd3a4ef77e6bbded364bf3f8030fc2b9
lucaphill <llucaphill@gmail.com>
2026-02-16 23:43:20 -0300

    concertando as progreções

--- atividadeWhile/progrecoes.c
+++ atividadeWhile/progrecoes.c
@@ -4,18 +4,51 @@ Programa: um programa que le a entrada do usuario e faz uma progreção aritmeti
 */
 #include<stdio.h>
 int main(void){
-    int c, t, r, n;
+    // definindo variaveis  
+    char c;
+    int t, r, n;
+    int progA, progG, soma;
+    soma = 0;
 
+    // pegando os valores
     puts("Voce quer fazer uma progressao aritmetica(a) ou geometrica (g)?");
-    scanf("%d",&c);
+    scanf("%c",&c);
 
-    if ( c == "a"){
+    printf("Digite o termo orientador: ");
+    scanf("%d",&t);
 
+    printf("Digite a razao da progressao: ");
+    scanf("%d",&r);
 
+    printf("Digite quantos numeros devem aparecer: ");
+    scanf("%d",&n);
 
-    };
+    // pogrecao aritmetica
+    if (c == 'a'){
 
+        printf("[ ");
+        for (int i = 1; i <= n; i++){
+            progA = t + (i*r);
+            
+            printf("%d, ", progA);
+            soma += progA;
+        }
 
+        printf("]\n");
+        printf("Soma: %d\n",soma);
+
+    } else if (c == 'g') { // progrecao geometrica
+
+        printf("[ ");
+        for (int j = 1; j <= n; j++){
+            progG = t * (j*r);
+            
+            printf("%d, ", progG);
+            soma += progG;
+        }
+        printf("]\n");
+        printf("Soma: %d\n",soma);
+    }
 
     return 0;
 }
