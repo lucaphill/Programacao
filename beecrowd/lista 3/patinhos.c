9c4e7ad87cc29d5311a402c64170c39f87c743d9
lucaphill <llucaphill@gmail.com>
2026-03-08 19:32:55 -0300

    deixando o vsCode feliz

--- beecrowd/lista 3/patinhos.c
+++ beecrowd/lista 3/patinhos.c
@@ -3,13 +3,14 @@ Luca Borges
 Programa: subtrai um pato 
 */
 #include <stdio.h>
+#include <limits.h>
 
 int main(void){
     unsigned long long int patos=0;
 
     while (1){
         scanf("%llu",&patos);
-        if (patos == 18446744073709551615){
+        if (patos == ULLONG_MAX){
             break;
         } else if (patos == 0){
             printf("%llu\n",patos);
