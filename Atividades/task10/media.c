ddd9c73243f9e3302a8b43bd73b17f5f5ede84be
lucaphill <llucaphill@gmail.com>
2026-03-07 21:59:54 -0300

    media (deixei mais dificil do que precisava)

---
+++ Atividades/task10/media.c
@@ -0,0 +1,38 @@
+/*
+Luca Borges
+Programa:
+*/
+#include <stdio.h>
+
+double AdicioneNaMedia(double valor){
+    static float soma = 0;
+    static int contador  = 1;
+    double total;
+
+    soma += valor;
+
+    total = soma / contador;
+    contador ++;
+    return total;
+}
+
+
+int main(void){
+    double valor, total;
+    int duracao;
+
+    puts("Insira a quantidade de rodadas:");
+        scanf("%d", &duracao);
+
+    for (int i = 0; i < duracao; i++){
+        puts("Insira um valor novo a media:");
+        scanf("%lf", &valor);
+        
+        total = AdicioneNaMedia(valor);
+        printf("A media atual eh: %.2lf\n", total);
+
+        
+    }
+    
+    return 0;
+}
---
+++ Atividades/task10/pitagoras.c
@@ -0,0 +1,12 @@
+/*
+Luca Borges
+Programa: verifica triangulos retangulos
+*/
+#include <stdio.h>
+
+int main(void){
+
+
+
+    return 0;
+}
