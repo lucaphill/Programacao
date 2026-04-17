1c457b8758e6e4b87cf569a810c1f513827a5e48
lucaphill <llucaphill@gmail.com>
2026-04-16 21:29:56 -0300

    variaveis basicas

--- C++/intro.cpp
+++ C++/intro.cpp
@@ -4,14 +4,24 @@ int main(void){
     // declarando variáveis
 
     int inteiro;
-    float flutuante;
-    char letra;
+    double flutuante; // use eles ao invez de float
+    char letra; // ainda é um unico caractere
+    bool binario; // tem na biblioteca padrão (:
+    std::string nome = "Luca Borges"; // NÃO ACREDITO, TEMOS STRINGS
 
     inteiro = 1000;
 
     // print (muito estranho)
-    std::cout << inteiro << '\n';
+    std::cout << "Olá " << nome << '\n';
 
+    // CONSTANTES
+    const double PI = 3.14; // mesma coisa de c SEMPRE USE ELAS
+    double raio = 10;
+    double circunferencia;
+
+    circunferencia = 2*PI*raio;
+
+    std::cout << circunferencia << '\n';
 
     return 0;
 }
